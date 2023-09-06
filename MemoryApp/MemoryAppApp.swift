36fa4452c10f55be753f6a9768dbeea76a9e1722
Burhan Faquiri <burhan_faquiri@hotmail.com>
2023-09-06 12:36:32 -0400

    Initial Commit

---
+++ MemoryApp/MemoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryAppApp.swift
+//  MemoryApp
+//
+//  Created by Burhan Faquiri on 2023-09-06.
+//
+
+import SwiftUI
+
+@main
+struct MemoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
